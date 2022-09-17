d9a879b6a897f27995cf8ef684be853d52e09264
vineethsagar <30525698+vineethsagar@users.noreply.github.com>
2022-09-17 06:29:07 +0530

    3 files modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,65 @@
 import * as React from 'react';
+import styled from 'styled-components/native/dist/index-standalone';
 import './style.css';
 
+const FaqDetails = styled.details`
+  font-size: 1rem;
+  color: hsl(0deg 0% 25%);
+  margin: 32px;
+  border: 2px solid;
+  border-radius: 4px;
+`;
+
+const FaqDiv = styled.div`
+background: hsl(0deg 0% 92.5%);
+border-radius: 0 0 4px 4px;
+padding: 12px 17px;
+font-style: italic;
+`;
+
+const FaqSummary = styled.summary`
+cursor: pointer;
+padding: 12px 16px;
+outline-offset: 5px;
+`;
+
+function FrequentlyAskedQuestion({ question, answer }) {
+  return (
+    <FaqDetails>
+      <FaqSummary>{question}</FaqSummary>
+      <FaqDiv>{answer}</FaqDiv>
+    </FaqDetails>
+  );
+}
+
+/**
+ * details.faq {
+  font-size: 1rem;
+  color: hsl(0deg 0% 25%);
+  margin: 32px;
+  border: 2px solid;
+  border-radius: 4px;
+}
+
+details.faq summary {
+  cursor: pointer;
+  padding: 12px 16px;
+  outline-offset: 5px;
+}
+
+details.faq .answer {
+  background: hsl(0deg 0% 92.5%);
+  border-radius: 0 0 4px 4px;
+  padding: 12px 17px;
+  font-style: italic;
+}
+
+ */
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <FrequentlyAskedQuestion
+      question="What does “CSS” stand for?"
+      answer="Cool Styling Strategy"
+    />
   );
 }
